c6368e0f9569db3623fea0f2579fc6a454465e4e
TomJerry-07 <125873376+TomJerry-07@users.noreply.github.com>
2023-05-09 14:49:32 +0530

    Add files via upload

---
+++ notes app 2/dis.js
@@ -0,0 +1,51 @@
+/*  <div class="box-content">
+        <p><b>jinjljljni</b> <br> jijnjinjn</p>
+    </div>
+*/
+
+let content = localStorage.getItem("selected");
+// console.log(content)
+let newDiv = document.createElement("div");
+newDiv.className = "box";
+newDiv.innerHTML = content;
+// console.log(newDiv)
+let tit = newDiv.querySelector("b");
+let context = newDiv.querySelector("p");
+let newcontext = context.textContent
+  .replace(context.querySelector("b").textContent, "")
+  .trim();
+let tittle = tit.innerText;
+// console.log(tittle)
+document.querySelector(".tittle").value = tittle;
+// console.log(newcontext)
+document.querySelector(".context").value = newcontext;
+
+let removeItem = { title: tittle, content: newcontext };
+
+function removeEle() {
+  let allList = JSON.parse(localStorage.getItem("output"));
+  // console.log(allList);
+  allList = allList.filter(
+    (obj) => JSON.stringify(obj) !== JSON.stringify(removeItem)
+  );
+  // console.log(removeItem);
+  localStorage.setItem("output", JSON.stringify(allList));
+  // console.log(allList);
+  window.location.href = "index.html";
+}
+function replaceEle() {
+  let newTittle = document.querySelector("input").value.trim();
+  let newTextarea = document.querySelector("textarea").value.trim();
+  let myArray = JSON.parse(localStorage.getItem("output")) || [];
+  let existingItem = { title: tittle, content: newcontext };
+  let newItem = { title: newTittle, content: newTextarea };
+  let index = myArray.findIndex(
+    (obj) =>
+      obj.title === existingItem.title && obj.content === existingItem.content
+  );
+  if (index !== -1) {
+    myArray.splice(index, 1, newItem);
+  }
+  localStorage.setItem("output", JSON.stringify(myArray));
+  window.location.href = "index.html";
+}
---
+++ notes app 2/val.js
@@ -0,0 +1,18 @@
+let button = document.querySelector("#add-to-list");
+button.addEventListener("click", ValuePasser);
+function ValuePasser() {
+  let content = document.querySelector("textarea");
+  let tittle = document.querySelector("input");
+  if (tittle.value == "") {
+    alert("Give any tittle to add");
+    return;
+  }
+  let tleVal = tittle.value.trim();
+  let cntVal = content.value.trim();
+  let Finaloutput = JSON.parse(localStorage.getItem("output")) || [];
+  Finaloutput.push({ title: tleVal, content: cntVal });
+  localStorage.setItem("output", JSON.stringify(Finaloutput));
+  content.value = "";
+  tittle.value = "";
+  window.location.href = "index.html";
+}
