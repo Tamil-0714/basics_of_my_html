280a1931401bf84de6b081432abe6afc0d51911e
TomJerry-07 <125873376+TomJerry-07@users.noreply.github.com>
2023-05-25 15:02:04 +0530

    app.js

--- form/app.js
+++ form/app.js
@@ -1,68 +1,87 @@
+// selection DOM elements 
 const form = document.querySelector("#form");
 const UserName = document.querySelector("#user-name");
 const Email = document.querySelector("#e-mail");
 const password = document.querySelector("#pass-word");
 const Cpassword = document.querySelector("#c-password");
-// const error =  document.querySelector(".error");
-let valid = false;
+
+let userValid = false;
+let mailValid = false;
+let passValid = false;
+let cPassValid = false;
+
+// submit event listener 
 form.addEventListener("submit", (e) => {
-  if (!validate()) e.preventDefault();
+  // validate every input element 
+  validate();
+  // check all the input elements are valid for prceed to submity
+  if (!(userValid && mailValid && passValid && cPassValid)) e.preventDefault();
 });
+
+// validate function()
 validate = () => {
   if (UserName.value.trim() == "") {
     setError(UserName, "User name must be entered ");
-    valid = false;
+    userValid = false;
   } else {
     setSucces(UserName);
-    valid = true;
+    userValid = true;
   }
   if (Email.value.trim() == "") {
     setError(Email, "Email Must be entered ");
-    valid = false;
+    mailValid = false;
   } else if (!validateEmail(Email.value)) {
     setError(Email, "Enter a valid Email");
-    valid = false;
+    mailValid = false;
   } else {
     setSucces(Email);
-    valid = true;
+    mailValid = true;
   }
   if (password.value.trim() == "") {
     setError(password, "Password must be enterd");
-    valid = false;
+    passValid = false;
   } else if (password.value.trim().length < 8) {
     setError(password, "Password must be greated than 8 charectors");
-    valid = false;
+    passValid = false;
   } else {
     setSucces(password);
-    valid = true;
+    passValid = true;
   }
-  if (Cpassword.value.trim() == "") {
+  if (Cpassword.value.trim() === "") {
     setError(Cpassword, "Confirm Password must be enterd");
-    valid = false;
+    cPassValid = false;
   } else if (Cpassword.value != password.value) {
-    setError(password, "Password and Confirm password are mis-match");
-    setError(Cpassword, "Password and Confirm password are mis-match");
-    valid = false;
+    setError(Cpassword, "Confirm password doesn't match ");
+    cPassValid = false;
   } else if (password.value.trim().length < 8) {
+    if (Cpassword.value.trim() !== "") {
+      setSucces(Cpassword);
+      cPassValid = true;
+    }
     setError(password, "Password must be greated than 8 charectors");
-    valid = false;
+    cPassValid = false;
   } else {
     setSucces(Cpassword);
-    valid = true;
+    cPassValid = true;
   }
-  return valid
 };
+
+// email validation funtion ()
 function validateEmail(email) {
   // Regular expression for email validation
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+//error set function ()
 setError = (element, errorMsg) => {
   const inputGroup = element.parentNode;
   const errorDiv = inputGroup.querySelector(".error");
   errorDiv.innerHTML = errorMsg;
   inputGroup.classList.add("error-contain");
 };
+
+// success set function ()
 setSucces = (element) => {
   const inputGroup = element.parentNode;
   const errorDiv = inputGroup.querySelector(".error");
