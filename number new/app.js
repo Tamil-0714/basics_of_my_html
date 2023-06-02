b1380f7c851a0cecc003b14ff99af7aba678983b
TomJerry-07 <125873376+TomJerry-07@users.noreply.github.com>
2023-06-02 08:58:35 +0530

    Update app.js

--- number new/app.js
+++ number new/app.js
@@ -214,10 +214,25 @@ let newobj = [
     console.log(newobj) 
  }
   UptateABS()
+function downloadTextFile(text, filename) {
+  const blob = new Blob([text], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
 
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
 // Usage example
 const myText = JSON.stringify(newobj);
+const myFilename = 'myFile.txt';
 function btnwrk(){
+    downloadTextFile(myText, myFilename);
     document.querySelector("div").innerHTML = myText
 }
 
