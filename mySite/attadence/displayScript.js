7ab6a2d298365b00b963fc655a8eadcb8dc9669d
TomJerry-07 <125873376+TomJerry-07@users.noreply.github.com>
2023-06-06 07:46:58 +0530

    Create displayScript.js

---
+++ mySite/attadence/displayScript.js
@@ -0,0 +1,57 @@
+let table = document.createElement('table'); 
+
+ table.classList.add('my-table'); 
+
+ let dummyJSON = localStorage.getItem('attadence_data') 
+
+ let newobj = JSON.parse(dummyJSON); 
+
+ let headingRow = document.createElement('tr'); 
+
+ let headingCell1 = document.createElement('th'); 
+
+ let headingCell2 = document.createElement('th'); 
+
+ headingCell1.textContent = 'D.No'; 
+
+ headingCell2.textContent = 'No:Days'; 
+
+ headingRow.appendChild(headingCell1); 
+
+ headingRow.appendChild(headingCell2); 
+
+ table.appendChild(headingRow); 
+
+ for (let i = 0; i < newobj.length; i++) { 
+
+   let row = document.createElement('tr'); 
+
+   let keyCell = document.createElement('td'); 
+
+   let valueCell = document.createElement('td'); 
+
+  
+
+   let key = Object.keys(newobj[i])[0]; 
+
+   let value = newobj[i][key]; 
+
+  
+
+   keyCell.textContent = key; 
+
+   valueCell.textContent = value; 
+
+  
+
+   row.appendChild(keyCell); 
+
+   row.appendChild(valueCell); 
+
+   table.appendChild(row); 
+
+ } 
+
+  
+
+ document.body.appendChild(table);
